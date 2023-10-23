4e57c9182d9b91537bce414a5cf7bed5bc104c56
Lukas Rost <git@lukasrost.de>
2023-10-23 11:33:19 +0000

    use platform-specific packages for sodium again

--- cmake/IncludeLibOpaque.cmake
+++ cmake/IncludeLibOpaque.cmake
@@ -6,7 +6,16 @@ option(GNU_MAKE_PATH "Path to GNU make" "make")
 
 message("Using make=${GNU_MAKE_PATH}")
 
+# resolves to something like build/lib/libopaque.a
+set(opaque_LIBRARY ${CMAKE_INSTALL_PREFIX}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}opaque${CMAKE_STATIC_LIBRARY_SUFFIX})
+
 include(ExternalProject)
+# do not overwrite CFLAGS if not debug build
+set(opaque_CFLAGS "")
+
+if(BUILD_TESTING)
+    set(opaque_CFLAGS "CFLAGS=\"-g -O0\"")
+endif()
 
 # directories for output library and headers
 make_directory(${CMAKE_BINARY_DIR}/lib)
@@ -20,7 +29,7 @@ GIT_SUBMODULES "" # update all submodules
 SOURCE_DIR        "${CMAKE_BINARY_DIR}/opaque-src"
 BUILD_IN_SOURCE TRUE
 CONFIGURE_COMMAND ""
-BUILD_COMMAND ${GNU_MAKE_PATH} -C ${CMAKE_BINARY_DIR}/opaque-src/src PREFIX=${CMAKE_BINARY_DIR} CFLAGS="${opaque_CFLAGS}"
+BUILD_COMMAND ${GNU_MAKE_PATH} -C ${CMAKE_BINARY_DIR}/opaque-src/src PREFIX=${CMAKE_BINARY_DIR} ${opaque_CFLAGS}
 INSTALL_COMMAND ${GNU_MAKE_PATH} -C ${CMAKE_BINARY_DIR}/opaque-src/src install PREFIX=${CMAKE_BINARY_DIR}
 BUILD_BYPRODUCTS ${opaque_LIBRARY}
 )
